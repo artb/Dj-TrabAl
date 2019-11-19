86bf0367e08fd227a8df784787587b6a4bb18060
Arthur Binda <artbinda@gmail.com>
2019-11-18 23:59:20 -0400

    Commit inicial

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/arthurbinda/Desktop/UFAM/TrabAlgII/Django/Dj-TrabAl/env/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
